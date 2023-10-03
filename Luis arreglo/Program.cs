9a89ba10d63f4140a5ecf1430cd7fe841b0597ee
Alex5 <Alex5@TOLEDO521>
2023-10-02 18:37:06 -0600

    Agregar archivos de proyecto.

---
+++ Luis arreglo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        char[] c = { 'L', 'u', 'i', 's' };
+        Console.WriteLine("el arreglo de caracteres es: ");
+        foreach (char letra in c)
+        {
+            Console.WriteLine(letra);
+        }
+        Console.WriteLine();
+
+        string nombre = new string(c);
+
+        Console.WriteLine("El nombre es: " + nombre);
+
+        char segundaLetra = c[1];
+        Console.WriteLine("la segunda letra es: " + segundaLetra);
+
+        c[3] = 'x';
+        Console.Write("el arreglo de caracter modificado es: ");
+        foreach (char letra in c)
+        {
+            Console.WriteLine(letra);
+        }
+        Console.WriteLine();
+
+        Console.ReadLine();
+    }
+}
